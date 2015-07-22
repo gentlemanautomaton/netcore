8f65451f2a42dd59237cee1c4ac0d1a9dacddbfe
Joshua Sjoding <netcore@josh.hey.nu>
2015-07-21 19:44:41 -0700

    Added stub for DNS testing harness

---
+++ dns_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/miekg/dns"
+)
+
+func TestWOL(t *testing.T) {
+	// TODO: Create database mockup that we can query?
+	m := new(dns.Msg)
+	m.SetQuestion("_wol.test", dns.TypeTXT)
+}
